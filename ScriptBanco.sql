9ae45ea086a662ab01b8b4292844f1c8e1cff061
kaua vitorio <kauavitorioof@gmail.com>
2021-03-01 01:46:08 -0300

    fix id

--- ScriptBanco.sql
+++ ScriptBanco.sql
@@ -11,24 +11,25 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 
 create database `bdecommerce`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
 use bdecommerce;
+drop database bdecommerce;
 
 CREATE TABLE `produtos` (
-  `id_produtos` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` INT(11) NOT NULL AUTO_INCREMENT,
   `nome_produto` VARCHAR(45) NOT NULL,
   `preco_produto` FLOAT(11) NOT NULL,
-  PRIMARY KEY (`id_produtos`))
+  PRIMARY KEY (`id_produto`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE `pedidos` (
-  `id_pedidos` INT(11) NOT NULL AUTO_INCREMENT,
-  `id_produtos` INT(11) NOT NULL,
+  `id_pedido` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` INT(11) NOT NULL,
   `quantidade` SMALLINT(6) NOT NULL,
-  PRIMARY KEY (`id_pedidos`),
-  INDEX `fk_pedidos_produtos_idx` (`id_produtos` ASC) VISIBLE,
+  PRIMARY KEY (`id_pedido`),
+  INDEX `fk_pedidos_produtos_idx` (`id_produto` ASC) VISIBLE,
   CONSTRAINT `fk_pedidos_produtos`
-    FOREIGN KEY (`id_produtos`)
-    REFERENCES `bdlivraria`.`produtos` (`id_produtos`)
+    FOREIGN KEY (`id_produto`)
+    REFERENCES `bdecommerce`.`produtos` (`id_produto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
