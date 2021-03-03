90b69aa8092bbcb74ca09859eeeeedf62124ab61
kaua vitorio <kauavitorioof@gmail.com>
2021-03-03 01:59:32 -0300

    canceled test

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const path = require('path');
 const login = require('../middleware/login');
 const ProdutosController = require('../controllers/produtos-controller');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, path.join(__dirname, '/uploads/'));
+        cb(null, './uploads/');
     },
     filename: function( req, file, cb ){
         let data = new Date().toISOString().replace(/:/g, '-') + '-';
