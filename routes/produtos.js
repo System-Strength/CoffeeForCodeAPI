a70cb10797a0ad2e89f47d59e09004097539ced2
kaua vitorio <kauavitorioof@gmail.com>
2021-03-06 15:32:25 -0300

    test #05

--- routes/produtos.js
+++ routes/produtos.js
@@ -6,7 +6,7 @@ const ProdutosController = require('../controllers/produtos-controller');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './uploads/');
+        cb(null, './uploads');
     },
     filename: function( req, file, cb ){
         let data = new Date().toISOString().replace(/:/g, '-');
