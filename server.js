bb9d166cd81011cbc27e143d3b466e69189d3006
kaua vitorio <kauavitorioof@gmail.com>
2021-03-09 10:45:50 -0300

    new setting

--- server.js
+++ server.js
@@ -4,10 +4,4 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
