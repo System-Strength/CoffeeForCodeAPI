ec74bbd9db254cb16da874e61c920256e939a845
kaua vitorio <kauavitorioof@gmail.com>
2021-03-11 14:43:37 -0300

    test #2

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -62,20 +62,21 @@ exports.getProductsByPopular = async (req, res, next) => {
         if (results.length == 0) {
             return res.status(204).send({ message: 'No Products popular' })
         }else{
-            return res.status(200).send(results.map(prods => {
+            const response ={ 
+            PopularProducts: results.map(prods => {
                     return {
-                        Search: {
-                            cd_prod: prods.cd_prod,
-                            img_prod: process.env.URL_API + prods.img_prod,
-                            nm_prod: prods.nm_prod,
-                            price_prod: prods.price_prod,
-                            qntd_prod: prods.qntd_prod,
-                            cd_cat: prods.cd_cat,
-                            date_prod: prods.date_prod,
-                            popular: prods.popular
-                        }
+                        cd_prod: prods.cd_prod,
+                        img_prod: process.env.URL_API + prods.img_prod,
+                        nm_prod: prods.nm_prod,
+                        price_prod: prods.price_prod,
+                        qntd_prod: prods.qntd_prod,
+                        cd_cat: prods.cd_cat,
+                        date_prod: prods.date_prod,
+                        popular: prods.popular
                 }
-        }))
+        })
+        }
+        return res.status(200).send(response);
         }
 
     }catch (error){
