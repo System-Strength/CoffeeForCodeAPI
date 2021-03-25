570bd224b97bc264fc222c171ed8681dc056705d
kaua vitorio <kauavitorioof@gmail.com>
2021-03-25 00:23:56 -0300

    set qt_prod

--- routes/shoppingcart.js
+++ routes/shoppingcart.js
@@ -6,7 +6,7 @@ router.get('/:email_user', ShoppingCartController.getCartOffUser);
 
 router.post('/insert/:email_user/:cd_prod/:nm_prod/:img_prod/:qt_prod/:price_unit_prod/:full_price_prod', ShoppingCartController.postIntenOnCart)
 
-router.patch('/updatecart/:email_user/:cd_prod/:full_price_prod', ShoppingCartController.patchItenCard)
+router.patch('/updatecart/:email_user/:cd_prod/:qt_prod/:full_price_prod', ShoppingCartController.patchItenCard)
 
 
 router.delete('/deleteItenCart/:email_user/:cd_prod/', ShoppingCartController.deleteItenCart)
