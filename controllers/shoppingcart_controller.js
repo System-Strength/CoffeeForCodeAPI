91ca28d6d48399e957cb82345f35ec089d0f66e9
kaua vitorio <kauavitorioof@gmail.com>
2021-03-25 12:10:37 -0300

    set id_user

--- controllers/shoppingcart_controller.js
+++ controllers/shoppingcart_controller.js
@@ -38,8 +38,8 @@ exports.postIntenOnCart = async (req, res, next ) => {
             return res.status(409).send({error: 'Just have this product on your cart'})
         }else{
             //  Insert products on user cart
-            const query = 'INSERT INTO tbl_shoppingcart(email_user, cd_prod, nm_prod, img_prod, qt_prod, price_unit_prod, full_price_prod) VALUES (?,?,?,?,?,?,?)'
-            const result = await mysql.execute(query, [ req.params.email_user, req.params.cd_prod, req.params.nm_prod, req.params.img_prod,
+            const query = 'INSERT INTO tbl_shoppingcart(id_user, email_user, cd_prod, nm_prod, img_prod, qt_prod, price_unit_prod, full_price_prod) VALUES (?,?,?,?,?,?,?,?)'
+            const result = await mysql.execute(query, [ req.params.id_user, req.params.email_user, req.params.cd_prod, req.params.nm_prod, req.params.img_prod,
                 req.params.qt_prod, req.params.price_unit_prod, req.params.full_price_prod ])
             return res.status(201).send({
                 email_user: result.email_user,
