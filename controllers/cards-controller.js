4725e0d80777e3566b6fd87492262ce4ee7df0ed
kaua vitorio <kauavitorioof@gmail.com>
2021-03-26 22:59:38 -0300

    create getcard

--- controllers/cards-controller.js
+++ controllers/cards-controller.js
@@ -1,20 +1,50 @@
 const mysql = require('../mysql')
 
-//  Insert new iten on shoppingCart
+
+exports.getCardOfUser = async (req, res, next) => {
+    try {
+        const query = 'select * from tbl_cards where email_user = ?;'
+        const resultcard = await mysql.execute(query, [req.params.email_user]) 
+    if(resultcard.length < 1){
+        return res.status(412).send({ warning: 'This user doesn`t have any card on account' })
+    }else {
+        const responsecard = {
+            length: resultcard.length,
+            Cards: resultcard.map(cards => {
+                return {
+                    cd_card: parseInt(cards.cd_card),
+                    email_user: cards.email_user,
+                    flag_card: cards.flag_card,
+                    number_card: cards.number_card,
+                    shelflife_card: cards.shelflife_card,
+                    cvv_card: cards.cvv_card,
+                    nmUser_card: cards.nmUser_card
+        }
+    })
+    }
+    return res.status(200).send(responsecard)
+    }
+    } catch (error) {
+        return res.status(500).send({ error: error })
+    }
+}
+
+//  Insert new Card for user
 exports.postNewCard = async (req, res, next ) => {
     try {
-        //  Verify if have same product on user cart
+        //  Check how many cards are registered
         const queryHave = 'select * from tbl_cards where email_user = ?;'
         const resultHaveOnCart = await mysql.execute(queryHave, [req.params.email_user]) 
 
         if(resultHaveOnCart.length >= 3){
             return res.status(207).send({error: 'User have more of 3 cards'})
         }else{
+            //  Check if have the same card on user account
             const querySameCard = 'select * from tbl_cards where number_card = ?;'
             const SameCard = await mysql.execute(querySameCard, [req.params.number_card] )
             if(SameCard.length > 0){
                 return res.status(409).send({error: 'Card already inserted'})
-            }else{//  Insert products on user cart
+            }else{//  Insert card for user
                 const query = 'INSERT INTO tbl_cards(email_user, flag_card, number_card, shelflife_card, cvv_card, nmUser_card) VALUES (?,?,?,?,?,?)'
                 const result = await mysql.execute(query, [ req.params.email_user, req.params.flag_card, req.params.number_card, req.params.shelflife_card,
                     req.params.cvv_card, req.params.nmUser_card ])
--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const CardsController = require('../controllers/cards-controller');
 
-router.get('/:email_user');
+router.get('/:email_user', CardsController.getCardOfUser);
 
 router.post('/insert/:email_user/:flag_card/:number_card/:shelflife_card/:cvv_card/:nmUser_card', CardsController.postNewCard)
 
